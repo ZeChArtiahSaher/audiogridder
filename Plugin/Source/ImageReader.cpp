c7e7cfa3e5a7073683597fdbf27a2c24f8c78f87
Zachar Kapustin <zachar.kapustin@packeta.com>
2022-09-21 10:32:09 +0200

    attempt simplify interpolation

--- Plugin/Source/ImageReader.cpp
+++ Plugin/Source/ImageReader.cpp
@@ -156,7 +156,7 @@ bool ImageReader::initCodec() {
                          m_outputFrame->height, 1);
 
     m_swsCtx = sws_getContext(m_widthPadded, m_heightPadded, m_inputCodecCtx->pix_fmt, m_width, m_height, fmt,
-                              SWS_BICUBIC, nullptr, nullptr, nullptr);
+                              SWS_BILINEAR, nullptr, nullptr, nullptr);
 
     logln("ready to process image stream with resolution: " << m_width << "x" << m_height << " *" << m_scale);
 
